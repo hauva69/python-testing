95455c88d04b133786c88dd4829d5cbb1e59d9a2
Ari Makela <ari.makela@iki.fi>
2023-03-29 06:55:27 +0300

    removed an extra ) and thus fixed a syntax error

--- sqlite3/sqlite.py
+++ sqlite3/sqlite.py
@@ -14,7 +14,7 @@ INSERT_SQL = """
         ('Monty Python and the Holy Grail', 1975, 8.2),
         ('And Now for Something Completely Different', 1971, 7.5)
 """
-SELECT_SQL = "SELECT score FROM movie")
+SELECT_SQL = "SELECT score FROM movie"
 
 def _main():
     conn = sqlite3.connect(DB_NAME)
