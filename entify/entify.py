0b81786b3716efc835cdaa9935d4450d14ee4c31
Ari Makela <ari.makela@iki.fi>
2023-04-11 14:39:29 +0300

    moved the conversion table from the test_entify to entify

--- entify/entify.py
+++ entify/entify.py
@@ -27,6 +27,28 @@ Transform accented characters to entities.
 
 import sys
 
+
+CONVERSION = {
+    'Á': '&#xC1;',
+    'á': '&#xE1;',
+    'É': '&#xC9;',
+    'é': '&#xE9;',
+    'È': '&#xC8;',
+    'è': '&#xE8;',
+    'Ò': '&#xD2;',
+    'ò': '&#xF2;',
+    'Ó': '&#xD3;',
+    'ó': '&#xF3;',
+    'Ü': '&#xDC;',
+    'ü': '&#xFC;',
+    'å': '&#xE5;',
+    'Å': '&#xC5;',
+    'Ä': '&#xC4;',
+    'ä': '&#xE4;',
+    'ö': '&#xF6;',
+    'Ö': '&#xD6;',
+}
+
 def entify(string: str) -> str:
     '''Transforms accented characters of a string to entities.'''
     return 'FIXME'
