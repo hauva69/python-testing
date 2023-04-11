b11901a278af8048122567095de53d9018f130bb
Ari Makela <ari.makela@iki.fi>
2023-04-11 14:21:19 +0300

    added test test_entify

---
+++ entify/test_entify.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+'''
+Tests for entify.
+
+| Merkki | Entiteetti |
+| ------ | ---------- |
+| Á      | &#xC1;     |
+| á      | &#xE1;     |
+| É      | &#xC9;     |
+| é      | &#xE9;     |
+| È      | &#xC8;     |
+| è      | &#xE8;     |
+| Ò      | &#xD2;     |
+| ò      | &#xF2;     |
+| Ó      | &#xD3;     |
+| ó      | &#xF3;     |
+| Ü      | &#xDC;     |
+| ü      | &#xFC;     |
+| å      | &#xE5;     |
+| Å      | &#xC5;     |
+| Ä      | &#xC4;     |
+| ä      | &#xE4;     |
+| Ö      | &#xF6;     |
+| ö      | &#xD6;     |
+'''
+
+import unittest
+
+from entify import entify
+
+class TestEntify(unittest.TestCase):
+    def test_entify(self):
+        test_string = 'Åke Bjärne Österman. Äldre konstapel.'
+        expected = '&#xC5;ke Bj&#xC5;rne &#xF6;sterman'
+        self.assertEqual(entify(test_string), expected, 'expected {0}'.format(expected))
+
+if __name__ == '__main__':
+    unittest.main()
