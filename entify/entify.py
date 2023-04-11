c7b65ca476d071b7a4c5d3a7cc5852e21d5c1c4b
Ari Makela <ari.makela@iki.fi>
2023-04-11 14:09:56 +0300

    Bjärjne => Bjärne

--- entify/entify.py
+++ entify/entify.py
@@ -2,6 +2,27 @@
 
 '''
 Transform accented characters to entities.
+
+| Merkki | Entiteetti |
+| ------ | ---------- |
+| Á      | &#xC1;     |
+| á      | &#xE1;     |
+| É      | &#xC9;     |
+| é      | &#xE9;     |
+| È      | &#xC8;     |
+| è      | &#xE8;     |
+| Ò      | &#xD2;     |
+| ò      | &#xF2;     |
+| Ó      | &#xD3;     |
+| ó      | &#xF3;     |
+| Ü      | &#xDC;     |
+| ü      | &#xFC;     |
+| å      | &#xE5;     |
+| Å      | &#xC5;     |
+| Ä      | &#xC4;     |
+| ä      | &#xE4;     |
+| ö      | &#xF6;     |
+| Ö      | &#xD6;     |
 '''
 
 import sys
@@ -11,4 +32,4 @@ def entify(string: str):
     return 'FIXME'
 
 if __name__ == '__main__':
-    entify('Åke Bjärjne Österman. Äldre konstapel.')
+    entify('Åke Bjärne Österman. Äldre konstapel.')
