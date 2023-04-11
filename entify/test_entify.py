2e205a615155bcf791d1b7888bc4325ca4d548f9
Ari Makela <ari.makela@iki.fi>
2023-04-11 14:32:15 +0300

    switched ö and Ö in the table and added the conversion dict

--- entify/test_entify.py
+++ entify/test_entify.py
@@ -21,13 +21,35 @@ Tests for entify.
 | Å      | &#xC5;     |
 | Ä      | &#xC4;     |
 | ä      | &#xE4;     |
-| Ö      | &#xF6;     |
-| ö      | &#xD6;     |
+| ö      | &#xF6;     |
+| Ö      | &#xD6;     |
 '''
 
 import unittest
 
 from entify import entify
+
+CONVERSION = {
+    'Á': '&#xC1;',
+    'á': '&#xE1;',
+    'É': '&#xC9;',
+    'é': '&#xE9;',
+    'È': '&#xC8;',
+|   'è': '&#xE8;',
+    'Ò': '&#xD2;',
+    'ò': '&#xF2;',
+    'Ó': '&#xD3;',
+    'ó': '&#xF3;',
+    'Ü': '&#xDC;',
+    'ü': '&#xFC;',
+    'å': '&#xE5',
+    'Å': '&#xC5;',
+    'Ä': '&#xC4;',
+    'ä': '&#xE4;',
+    'ö': '&#xF6;',
+    'Ö': '&#xD6;',
+'''
+}
 
 class TestEntify(unittest.TestCase):
     def test_entify(self):
