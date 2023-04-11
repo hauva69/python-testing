ff2a6b314facace7fa970990e4bad6d2c412f4fe
Ari Makela <ari.makela@iki.fi>
2023-04-11 13:17:48 +0300

    entify files added

---
+++ entify/README.md
@@ -0,0 +1,10 @@
+# Entify
+
+[HTML-entiteettejä](https://www.w3schools.com/charsets/ref_utf_latin1_supplement.asp)
+
+| Merkki | Entiteetti |
+| ------ | ---------- |
+| Ä      | &#xC4;     |
+| ä      | &#xE4;     |
+| ö      | &#xF6;     |
+| Ö      | &#xD6;     |
---
+++ entify/entify.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+'''
+Transform accented characters to entities.
+'''
+
+import sys
+
+def entify(string: str):
+    '''Transforms accented characters of a string to entities.'''
+    return 'FIXME'
+
+if __name__ == '__main__':
+    entify('Åke Bjärjne Österman. Äldre konstapel.')
