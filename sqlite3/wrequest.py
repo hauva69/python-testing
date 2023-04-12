bba08b2f00577cc998349a28d5a1f658314a8eb0
Ari Makela <ari.makela@iki.fi>
2023-04-12 23:48:57 +0300

    wrequest.py added

---
+++ sqlite3/wrequest.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+'''Checks whether the user exists in the database and has same priviledges.'''
+
+def _main():
+    print('hello, world')
+
+if __name__ == '__main__':
+    _main()
