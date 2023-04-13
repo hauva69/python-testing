6aeb1c12eb8ec0e85eabdabcedd7559d19cf6019
Ari Makela <ari.makela@iki.fi>
2023-04-13 07:51:42 +0300

    init the db

--- sqlite3/initdb.py
+++ sqlite3/initdb.py
@@ -4,13 +4,28 @@
 Init the database.
 '''
 
-import pathlib
+import logging
 import sqlite3
+
+logging.basicConfig(level=logging.INFO)
 
 DB_NAME = 'requests.db'
 
+def _get_connection():
+    return sqlite3.connect(DB_NAME)
+
 def _main():
-    pass
+    conn = _get_connection()
+    cursor = conn.cursor()
+    sql = '''CREATE TABLE requests(UID, RESPONSIBILITY, PRIMARY_POSITION, CREATED)'''
+
+    try:
+        cursor.execute(sql)
+    except sqlite3.OperationalError as ex:
+        logging.warning(ex)
+
+    cursor.close()
+    conn.close()
 
 if __name__ == '__main__':
     _main()
